915cb02adb4df6cabf13c4b0c8e415ceff416ae1
Raul de Arriba <rauldearriba@gmail.com>
2024-06-04 08:13:42 +0200

    end llm

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,37 +1,68 @@
+from typing import Tuple
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import (
+    Summary,
+    IceBreaker,
+    TopicOfInterest,
+    summary_parser,
+    ice_breaker_parser,
+    topics_of_interest_parser,
+)
 
-
-def ice_breaker_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, TopicOfInterest, IceBreaker, str]:
     linkedin_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
 
     summary_template = """
-        given the information {information} about a person from I want to create:
+        given the information {information} about a person I want to create:
         1. a short summary
         2. two interesting facts about them
+        \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = summary_prompt_template | llm
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/rauldearriba/")
-    res = chain.invoke(input={"information": linkedin_data})
+    chain = summary_prompt_template | llm | summary_parser
+    res: Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    topics_template = """
+        given the information {information} about a person, list their topics of interest.
+        \n{format_instructions}
+    """
+    
+    topics_prompt_template = PromptTemplate(
+        input_variables=["information"], template=topics_template,
+        partial_variables={"format_instructions": topics_of_interest_parser.get_format_instructions()}
+    )
+
+    topics_chain = topics_prompt_template | llm | topics_of_interest_parser
+    topics_of_interest: TopicOfInterest = topics_chain.invoke(input={"information": linkedin_data})
+
+    ice_breakers_template = """
+        given the information {information} about a person, suggest some ice breakers.
+        \n{format_instructions}
+    """
+    
+    ice_breakers_prompt_template = PromptTemplate(
+        input_variables=["information"], template=ice_breakers_template,
+        partial_variables={"format_instructions": ice_breaker_parser.get_format_instructions()}
+    )
+
+    ice_breakers_chain = ice_breakers_prompt_template | llm | ice_breaker_parser
+    ice_breakers: IceBreaker = ice_breakers_chain.invoke(input={"information": linkedin_data})
+
+    return res, topics_of_interest, ice_breakers, linkedin_data.get("profile_pic_url")
 
 if __name__ == '__main__':
     load_dotenv()
-
     print('Ice Breaker')
-    ice_breaker_with("Raul de Arriba jaume viladoms")
-    
-    
+    ice_break_with("Raul de Arriba jaume viladoms")
